3d6448106bb7b2ad75d7ffeffcb4925e3ef77a62
David <david.khayutin@gmail.com>
2018-10-12 00:01:13 +0000

    added MongoDB

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,23 @@
 $(() => {
 
-function loadAllTweets(){
-  $.ajax("/tweets").then((product) => {
-    renderTweets(product);
-  })
-}
-
-
-    $('.new-tweet form').on("submit", (info)=>{
-
-      info.preventDefault();
-      let charactersEntered = $('.new-tweet form textarea').val().length
-      let data = $(info.target).serialize();
-      if(charactersEntered === 0) {
-        $(".new-tweet form .error").text("You must enter something into a tweet!").addClass("errorTweet")
-        return;
-      }
-      if(charactersEntered > 140){
-        $(".new-tweet form .error").text("Your tweet is too long. Please Shorten!").addClass("errorTweet")
-        return;
-      }
-
-      $.ajax("/tweets", {method: 'POST', data: data}).then(() =>{
-        $(".new-tweet form")[0].reset();
-        $(".counter").text(140);
-        loadAllTweets();
-      })
-
-    })
-
   function createTweetElement(tweetObject){
     return $("<article>").addClass("tweeted")
-        .append($("<header>")
-          .append($("<img>").attr("src", tweetObject.user.avatars.small))
-          .append($("<h4>").text(tweetObject.user.name))
-          .append($("<h6>").text(tweetObject.user.handle))
-        )
-        .append($("<div>").text(tweetObject.content.text))
-        .append($("<footer>")
-          .append($("<h6>").text(tweetObject.created_at))
-          .append($("<ul>")
-            .append($("<li>").append($("<span>").append($("<i>").addClass("fa fa-heart"))))
-            .append($("<li>").append($("<span>").append($("<i>").addClass("fa fa-retweet"))))
-            .append($("<li>").append($("<span>").append($("<i>").addClass("fa fa-flag"))))
+      .append($("<header>")
+        .append($("<img>").attr("src", tweetObject.user.avatars.small))
+        .append($("<h4>").text(tweetObject.user.name))
+        .append($("<h6>").text(tweetObject.user.handle))
+      )
+      .append($("<div>").text(tweetObject.content.text))
+      .append($("<footer>")
+        .append($("<h6>").text(tweetObject.created_at))
+        .append($("<ul>")
+          .append($("<li>").append($("<span>").append($("<i>").addClass("fa fa-heart"))))
+          .append($("<li>").append($("<span>").append($("<i>").addClass("fa fa-retweet"))))
+          .append($("<li>").append($("<span>").append($("<i>").addClass("fa fa-flag"))))
 
-          )
         )
-  }
+      )
+    }
 
   function renderTweets (data) {
     $(".tweetfeed").empty();
@@ -57,18 +28,63 @@ function loadAllTweets(){
     }
   }
 
-  loadAllTweets();
-//deligation
+  function loadAllTweets(){
+    $.ajax("/tweets").then((product) => {
+      renderTweets(product);
+    })
+  }
 
-  $(".tweetfeed").on('click', "li span .fa-heart",(event) =>{
-    $(event.target).toggleClass("toManyCharacters");
+  loadAllTweets();
+
+   $(".new-tweet").slideToggle(0);
+
+  // $(".tweetfeed").on('click', "li span .fa-heart",(event) =>{
+  //   $(event.target).toggleClass("toManyCharacters");
+  // })
+
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+     console.log($(".new-tweet").css("display"))
+    if(scroll > 230 && $(".new-tweet").css("display") === "inline-block"){
+      $(".new-tweet").slideUp();
+    }
+  });
+
+  $(".followMe").click(function(){
+    var scroll = $(window).scrollTop();
+    $(".followMe").toggle().text("ON")
+
   })
 
 
   $("nav button").click(function(){
-    console.log("click")
-    $(".new-tweet").toggle();
+    $(".new-tweet").slideToggle("slow");
     $(".new-tweet input:text, .new-tweet textarea").first().focus();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  })
+
+
+  $('.new-tweet form').on("submit", (info)=>{
+
+    info.preventDefault();
+    let charactersEntered = $('.new-tweet form textarea').val().length
+    let data = $(info.target).serialize();
+    if(charactersEntered === 0) {
+      $(".new-tweet form .error").text("You must enter something into a tweet!").addClass("errorTweet")
+      return;
+    }
+    if(charactersEntered > 140){
+      $(".new-tweet form .error").text("Your tweet is too long. Please Shorten!").addClass("errorTweet")
+      return;
+    }
+
+    $.ajax("/tweets", {method: 'POST', data: data}).then(() =>{
+      $(".new-tweet form")[0].reset();
+      $(".counter").text(140);
+      $(".new-tweet").slideToggle(0);
+      loadAllTweets();
+    })
   })
 
 });
